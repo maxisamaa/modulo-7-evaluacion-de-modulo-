19f88c56a3b84d2d8b68d7c2562f21c55894c14e
Maximiliano Garrido <maximiliano.garrido@ug.uchile.cl>
2025-11-11 14:17:04 -0300

    se agrego el proyecto2

--- etiquetas/admin.py
+++ etiquetas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Etiqueta
+
+admin.site.register(Etiqueta)
--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Producto, Detalles
+from etiquetas.models import Etiqueta
 
-# Register your models here.
+class DetallesInline(admin.StackedInline):
+    model = Detalles
+    extra = 0
+
+class EtiquetaInline(admin.TabularInline):
+    model = Etiqueta.productos.through  # usa la tabla intermedia del ManyToMany
+    extra = 1
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'categoria')
+    search_fields = ('nombre', 'descripcion')
+    list_filter = ('categoria',)
+    inlines = [DetallesInline, EtiquetaInline]
+
+    # evita mostrar el campo intermedio duplicado
+    exclude = ('etiquetas',)
+
+# Registro normal de Detalles
+admin.site.register(Detalles)
